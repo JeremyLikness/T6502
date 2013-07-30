5a804c41939c1310a7de6139c0d10f79e8285cde
Jeremy Likness <jeremy@jeremylikness.com>
2013-07-30 23:26:06 +0000

    Limit console size

--- Source/T6502/Scripts/app/services/consoleService.ts
+++ Source/T6502/Scripts/app/services/consoleService.ts
@@ -1,4 +1,5 @@
 ///<reference path="../app.ts"/>
+///<reference path="../globalConstants.ts"/>
 
 module Services {
     
@@ -17,6 +18,11 @@ module Services {
 
         public log(message: string) {
             this.lines.push(message);
+
+            if (this.lines.length > Constants.Display.ConsoleLines) {
+                this.lines.splice(0, 1);
+            }
+            
         }
 
     }
